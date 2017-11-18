25ce4952eaae9579f9f694927760fae40b2124e8
mamun <mamunthecoder@gmail.com>
2017-11-19 00:06:19 +0600

    angular integration

---
+++ public/js/app.ng.js
@@ -0,0 +1,18 @@
+var app = angular.module("myApp", ["ngRoute"]);
+app.run(function ($rootScope) {
+    $rootScope.site_title = "LUXURY WATCHES";
+}).config(function ($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: "templates/index.html"
+        })
+        .when("/details", {
+            templateUrl: "templates/details.html"
+        })
+        .when("/checkout", {
+            templateUrl: "templates/checkout.html"
+        })
+        .when("/contact", {
+            templateUrl: "templates/contact.html"
+        });
+});
