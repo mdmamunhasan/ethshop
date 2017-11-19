2da9aa10af48b3d7f8af80982ceab66af5c1f6ac
mamun <mamunthecoder@gmail.com>
2017-11-19 12:17:05 +0600

    angular integration

---
+++ public/js/controllers/details.js
@@ -0,0 +1,18 @@
+app.controller('detailsCtrl', function ($scope) {
+    $scope.togglePane = function ($event) {
+        $event.preventDefault();
+
+        var element = angular.element($event.target),
+            sibling = $(element).parent().find('ul').first(),
+            current = $(sibling).hasClass('hide');
+
+        $('.menu_drop > li > ul').addClass('hide');
+
+        if(current){
+            $(sibling).removeClass('hide');
+        }
+        else{
+            $(sibling).addClass('hide');
+        }
+    };
+});
