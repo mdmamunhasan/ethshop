fece78a6a4720318ae2e0106f98066c21a4a4c62
mamun <mamunthecoder@gmail.com>
2017-11-20 01:15:56 +0600

    web3 placeorder

--- public/js/controllers/orders.js
+++ public/js/controllers/orders.js
@@ -1,11 +1,14 @@
 app.controller('ordersCtrl', function ($scope, $timeout) {
+
+    $scope.userAddress = '';
     $scope.orderList = [];
 
     $timeout(function () {
+        $scope.userAddress = App.account;
         App.getUserOrderList(null, function (result) {
             if (result.orderId) {
                 $scope.orderList.push(result);
-                console.log($scope.orderList);
+                $scope.$applyAsync();
             }
         });
     }, 1000);
